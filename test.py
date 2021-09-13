b3c9a3e24b3b1a2058490f7e7c8cf9e206c190cf
edoardo.fantolino <68923497+edoardofantolino@users.noreply.github.com>
2021-09-13 16:53:01 +0200

    Add files via upload

---
+++ test.py
@@ -0,0 +1,7 @@
+import torch
+import torchvision
+import torchvision.transforms as transforms
+import torch.optim as optim
+from architecture.model import Net
+import torch.nn as nn
+import torch.nn.functional as F
