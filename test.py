b8e8550fa5ff079dec46956801c5ab4f8d5f16e1
edoardo.fantolino <68923497+edoardofantolino@users.noreply.github.com>
2021-09-13 17:36:03 +0200

    Update test.py

--- test.py
+++ test.py
@@ -5,3 +5,41 @@ import torch.optim as optim
 from architecture.model import Net
 import torch.nn as nn
 import torch.nn.functional as F
+from PIL import Image
+import matplotlib.pyplot as plt
+
+
+transform = transforms.Compose([
+        transforms.RandomResizedCrop(32, (0.5, 2.0)),
+        transforms.RandomHorizontalFlip(), 
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                  std=[0.229, 0.224, 0.225]),
+    ])
+
+net = Net()
+m_state_dict = torch.load('/content/firstCNN/cifar_net.pth')
+net.load_state_dict(m_state_dict)
+
+img = Image.open("/content/firstCNN/cano.jpg")
+img = img.resize((32,32))
+
+plt.imshow(img)
+plt.show()
+
+
+img = transform(img)
+img = img.unsqueeze(0)
+print(img.shape)
+print(img)
+
+net.eval()
+output = net(img)
+print(output)
+print(output.shape)
+
+
+
+
+
+
